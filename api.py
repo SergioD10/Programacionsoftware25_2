7edf8cb949aedcaac6031fc1fc2ef0199f4d0c3e
SergioD10 <sergiodiosa10@gmail.com>
2025-08-14 11:05:55 -0500

    feat:Creacion de api1

---
+++ api.py
@@ -0,0 +1,5 @@
+def restar(a:int, b:int)->int:
+    return a - b
+
+resultado = restar(5, 2)
+print("La resta es:", resultado)
---
+++ test.py
@@ -0,0 +1,5 @@
+def sumar(a:int, b:int)->int:
+    return a + b
+
+resultado = sumar(5, 2)
+print("El resulado de la suma es:", resultado)
